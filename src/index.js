a5dd2beeb898aaf7e2c57637118dfdcdb880a37f
Konstantina Boua <85695279+KonsBo@users.noreply.github.com>
2024-08-29 20:48:32 +0300

    Added basic form handling and added  Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings: [
+      "Time is one of the few things we regard as regular and unchanging.Time works by measuring periods between the past, present and future.",
+    ],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+// function generateImage(event) {
+//     event.preventDefault();
+//     let imageElement = document.querySelector("#image");
+//     imageElement.innerHTML =
+
+// }
+
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
+
+// let imageFormElement = document.querySelector("#image-generator");
+// imageFormElement.addEventListener("submit", generateImage);
