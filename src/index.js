9f63a4e0c1efa1c59915ab328c016f908396150d
Konstantina Boua <85695279+KonsBo@users.noreply.github.com>
2024-08-30 12:26:03 +0300

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
-function generateQuote(event) {
-  event.preventDefault();
-
+//Display the generated quote
+function displayQuote(response) {
+  console.log("quote generated");
+  //response.data.answer
   new Typewriter("#quote", {
-    strings: [
-      "Time is one of the few things we regard as regular and unchanging.Time works by measuring periods between the past, present and future.",
-    ],
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateQuote(event) {
+  event.preventDefault();
+
+  //Build the API URL
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "fo0aeaadb1171c3bat01accf9c25f94b";
+
+  let context =
+    "You are a quote expert and you can give quotes of every author in the world. Especially quotes froma poets, scientists, philosophers, academics in general. Your mission is to generate a top 5 line quote providing it with a HTML format. If the instruction given by the user is unknown, add a quote from an existing one, saying first politely that you can't find a quote with the topic that is given. Do not provide the same quote from the same person more than once. Please follow the users' instructions. You just have to include the word that is given by the user, finding a topic. If you find the instructed word just give the answer and the name, don't say anything else.";
+
+  let prompt = `Users' instructions: Generate a quote from ${instructionsInput.value}.Provide the authors' name into a <strong> element.`;
+
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating Quote");
+  console.log(`Prompt:${prompt}`);
+  console.log(`Context:${context}`);
+
+  //Make a call to the API
+  axios.get(apiURL).then(displayQuote);
 }
 
 // function generateImage(event) {
